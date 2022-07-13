cc1c52f26cc2cebd00cc434fbc49daf5b2e9b95f
Valerde <602533622@qq.com>
2022-07-14 01:02:41 +0800

    maven 了解

---
+++ maven_demo/src/main/java/com/sovava/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sovava;
+
+/**
+ * Description: maven 项目学习
+ *
+ * @author: ykn
+ * @date: 2022年07月14日 0:11
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello Maven~");
+
+//        Driver
+    }
+
+}
---
+++ maven_demo/src/test/java/test1.java
@@ -0,0 +1,16 @@
+import com.mysql.jdbc.Driver;
+import junit.*;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2022年07月14日 0:34
+ **/
+public class test1 {
+//    @Test
+    public static void main(String[] args) {
+        System.out.println("test");
+//        Driver
+    }
+}
